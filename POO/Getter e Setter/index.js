10808f3a6e3a818ff220deedd487e50b1bf7ecfe
mbranches <marcusvbranches@gmail.com>
2025-04-29 22:18:31 -0300

    feat: adiciona aula de getter e setter

---
+++ POO/Getter e Setter/index.js
@@ -0,0 +1,44 @@
+const _velocidade = Symbol('velocidade');
+
+class Carro {
+    constructor(nome) {
+        this.nome = nome;
+        this[_velocidade] = 0;
+    }
+    
+    set velocidade(valor) {
+        if(typeof valor !== "number") return;
+        if(valor >= 100 || valor <= 0) return; 
+        this[_velocidade] = valor;
+    }
+
+    get velocidade() {
+        return this[_velocidade];
+    }
+
+    acelerar() {
+        if(this[_velocidade] >= 100) return;
+
+        this[_velocidade]++;
+    }
+
+    freiar() {
+        if(this[_velocidade] <= 0) return;
+
+        this[_velocidade]--;
+    }
+}
+
+const c1 = new Carro("fusca");
+
+for(let i = 0; i <= 200; i++) {
+    c1.acelerar();
+}
+
+c1.velocidade = 1010;
+
+console.log(c1.velocidade);
+
+c1.velocidade = 10;
+
+console.log(c1.velocidade)
