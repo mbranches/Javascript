e3ddaaa61678348acd4c4c740b4e36be56e4e9db
mbranches <marcusvbranches@gmail.com>
2025-05-08 17:52:24 -0300

    feat: adiciona aula de como selecionar o pai de um elemento

---
+++ DOM/03 - Encontrando Elementos/00 - Elemento pai/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const strong = document.querySelector("strong");
+console.log(strong.parentElement);
+console.log(strong.parentNode);
+
+const p = document.querySelector(".paragrafo");
+console.log(p.parentElement)
