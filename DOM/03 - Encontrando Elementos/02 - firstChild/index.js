e7a1136a5052d4c40b837f90f4f9a6f7452e8e1c
mbranches <marcusvbranches@gmail.com>
2025-05-10 10:51:58 -0300

    feat: adiciona aula sobre firstChild e firstElementChild

---
+++ DOM/03 - Encontrando Elementos/02 - firstChild/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const container = document.querySelector(".container");
+
+console.log(container.firstChild);
+console.log(container.firstElementChild);
+
+const p = document.querySelector(".p");
+console.log(container.firstChild);
+console.log(container.firstElementChild);
