f4cc3411dd6c529fc808ee62ac0f713b5212d5c0
mbranches <marcusvbranches@gmail.com>
2025-05-16 17:38:17 -0300

    feat: adiciona aula sobre preventDefault

---
+++ DOM/04 - Eventos/04 - preventDefault/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const link = document.querySelector(".link");
+
+link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    console.log('Meu linkedin é top');
+
+    event.deg
+})
